1042e80c91a3e57a564fbab17656560504dc4f0f
Dan Hansen <dchansen54@gmail.com>
2022-08-06 13:15:28 -0400

    change

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function ask(questionText) {
 
 start();
 
-async function start() {
+async function start(){
+
+
   console.log("Let's play a game where you (human) make up a number between 1 and 100 and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
@@ -17,30 +19,38 @@ async function start() {
 
   // Computer takes first guess
   var number = Math.floor(Math.random() * 100 + 1)
-console.log("is your secret number" + number) 
+  console.log("Is your secret number " + number) 
+   // human responds
+   let answer = await ask( "Y or 'N")
+  console.log("You answered "  +  answer);
+  if (answer === "Y"){
+    console.log("You Win!!!")
+  }else if (answer === "N"){
+    console.log("Wrong, Guess again loser")
+  }
 
-// If Computer answered correctly
+  // computer will guess again
 
-if number == secretNumber {
-  console.log(" Congrats, you win!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   process.exit();
+
 }
-
-// If Computer was incorrect 
-ask
-
-
-
-
-
-
-
-
-
-
-  process.exit();
-
-}
-
-
 
 
