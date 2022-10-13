651495ace460cdc3b01ec2e406fc485d2624d720
Dan Hansen <dchansen54@gmail.com>
2022-10-13 19:46:44 -0400

    2 more points baby

--- index.js
+++ index.js
@@ -11,25 +11,29 @@ start();
 
 async function start(){
 
-
+// Beginning of game will ask the player to guess a number. 
   console.log("Let's play a game where you (human) make up a number between 1 and 100 and I (computer) try to guess it.")
+
+  // This will record the secret number and remember it
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
 
   // Computer takes first guess
   var number = Math.floor(Math.random() * 100 + 1)
-  console.log("Is your secret number " + number) 
-   // human responds
-   let answer = await ask( "Y or 'N")
-  console.log("You answered "  +  answer);
-  if (answer === "Y"){
-    console.log("You Win!!!")
-  }else if (answer === "N"){
-    console.log("Wrong, Guess again loser")
-  }
+  //console.log("Is your secret number " + number) 
 
-  // computer will guess again
+   // human responds with Yes or No
+   
+   let answer = await ask("(Y)es or (N)o is your secret number " + number)
+  console.log("you answered" + answer)
+  if (answer === "Y"){ 
+  console.log("I Win")
+  // If user says no computer will ask higher or lower
+  }else if (answer === "N"){
+    let guess = await ask("Is it (H)igher or (L)ower?")
+  }else if (guess === "H")
+  
 
 
 
